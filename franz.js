ebd992b0d3831eba170a76674a21b3f73565da2b
Noah Morrow <noahmorrow07@mail.com>
2018-04-23 16:52:28 -0500

    sdas
    sdasd

---
+++ franz.js
@@ -0,0 +1,115 @@
+(function (lib, img, cjs, ss, an) {
+
+var p; // shortcut to reference prototypes
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+(lib.Fanzlogoalone = function() {
+	this.initialize(img.Fanzlogoalone);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,184,163);
+
+
+// stage content:
+(lib.Untitled1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_24 = function() {
+		/* Stop at This Frame
+		The  timeline will stop/pause at the frame where you insert this code.
+		Can also be used to stop/pause the timeline of movieclips.
+		*/
+		
+		this.stop(25);
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(24).call(this.frame_24).wait(1));
+
+	// Layer 5 (mask)
+	var mask = new cjs.Shape();
+	mask._off = true;
+	var mask_graphics_11 = new cjs.Graphics().p("AotA+IBHh8IQUAAIAAB8g");
+	var mask_graphics_12 = new cjs.Graphics().p("ApDCOICikbIPkBNIgmDOg");
+	var mask_graphics_13 = new cjs.Graphics().p("ApYDdID8m5IO2CbIhOEeg");
+	var mask_graphics_14 = new cjs.Graphics().p("ApvEsIFYpXIOHDnIh2Fwg");
+	var mask_graphics_15 = new cjs.Graphics().p("AqFF8IG0r3INXE1IidHCg");
+	var mask_graphics_16 = new cjs.Graphics().p("AqbHLIIPuVIMoGCIjEITg");
+	var mask_graphics_17 = new cjs.Graphics().p("AqxIaIJqwzIL5HPIjrJkg");
+	var mask_graphics_18 = new cjs.Graphics().p("ArHJqILGzTILJIdIkSK2g");
+	var mask_graphics_19 = new cjs.Graphics().p("ArdK5IMg1xIKbJqIk5MHg");
+	var mask_graphics_20 = new cjs.Graphics().p("ArzMIIN84PIJrK3IlgNYg");
+	var mask_graphics_21 = new cjs.Graphics().p("AsJNYIPW6vII9MFImHOqg");
+	var mask_graphics_22 = new cjs.Graphics().p("AsfOnIQy9NIINNSImuP7g");
+
+	this.timeline.addTween(cjs.Tween.get(mask).to({graphics:null,x:0,y:0}).wait(11).to({graphics:mask_graphics_11,x:100.3,y:189.8}).wait(1).to({graphics:mask_graphics_12,x:102,y:181.9}).wait(1).to({graphics:mask_graphics_13,x:103.7,y:173.9}).wait(1).to({graphics:mask_graphics_14,x:105.4,y:166}).wait(1).to({graphics:mask_graphics_15,x:107.1,y:158.1}).wait(1).to({graphics:mask_graphics_16,x:108.8,y:150.2}).wait(1).to({graphics:mask_graphics_17,x:110.5,y:142.2}).wait(1).to({graphics:mask_graphics_18,x:112.2,y:134.3}).wait(1).to({graphics:mask_graphics_19,x:113.9,y:126.4}).wait(1).to({graphics:mask_graphics_20,x:115.7,y:118.4}).wait(1).to({graphics:mask_graphics_21,x:117.4,y:110.5}).wait(1).to({graphics:mask_graphics_22,x:119.1,y:102.6}).wait(3));
+
+	// Layer 4
+	this.instance = new lib.Fanzlogoalone();
+	this.instance.parent = this;
+	this.instance.setTransform(8,19);
+	this.instance._off = true;
+
+	var maskedShapeInstanceList = [this.instance];
+
+	for(var shapedInstanceItr = 0; shapedInstanceItr < maskedShapeInstanceList.length; shapedInstanceItr++) {
+		maskedShapeInstanceList[shapedInstanceItr].mask = mask;
+	}
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(11).to({_off:false},0).wait(14));
+
+	// Layer 2 (mask)
+	var mask_1 = new cjs.Shape();
+	mask_1._off = true;
+	var mask_1_graphics_0 = new cjs.Graphics().p("AldgxIPJAAIgoBZIyvAKg");
+	var mask_1_graphics_1 = new cjs.Graphics().p("Ap5BBIErjFIPIAAIiCEJg");
+	var mask_1_graphics_2 = new cjs.Graphics().p("AqHBLIFIknIPHAAIjcG5g");
+	var mask_1_graphics_3 = new cjs.Graphics().p("AqVBVIFlmJIPGAAIk2Jpg");
+	var mask_1_graphics_4 = new cjs.Graphics().p("AqjBfIGCnrIPFAAImQMZg");
+	var mask_1_graphics_5 = new cjs.Graphics().p("AqxBpIGfpNIPEAAInqPJg");
+	var mask_1_graphics_6 = new cjs.Graphics().p("Aq/BzIG8qvIPDAAIpER5g");
+	var mask_1_graphics_7 = new cjs.Graphics().p("ArNB9IHZsRIPCAAIqeUpg");
+	var mask_1_graphics_8 = new cjs.Graphics().p("ArbCHIH2tzIPBAAIr3XZg");
+	var mask_1_graphics_9 = new cjs.Graphics().p("ArpCRIITvVIPAAAItRaJg");
+	var mask_1_graphics_10 = new cjs.Graphics().p("Ar3CbIIww3IO/AAIurc5g");
+
+	this.timeline.addTween(cjs.Tween.get(mask_1).to({graphics:mask_1_graphics_0,x:83,y:11.1}).wait(1).to({graphics:mask_1_graphics_1,x:81.5,y:19.4}).wait(1).to({graphics:mask_1_graphics_2,x:80,y:28.2}).wait(1).to({graphics:mask_1_graphics_3,x:78.5,y:37}).wait(1).to({graphics:mask_1_graphics_4,x:77,y:45.8}).wait(1).to({graphics:mask_1_graphics_5,x:75.5,y:54.6}).wait(1).to({graphics:mask_1_graphics_6,x:74,y:63.4}).wait(1).to({graphics:mask_1_graphics_7,x:72.5,y:72.2}).wait(1).to({graphics:mask_1_graphics_8,x:71,y:81}).wait(1).to({graphics:mask_1_graphics_9,x:69.5,y:89.8}).wait(1).to({graphics:mask_1_graphics_10,x:68,y:98.6}).wait(15));
+
+	// Logo
+	this.instance_1 = new lib.Fanzlogoalone();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(8,19);
+
+	var maskedShapeInstanceList = [this.instance_1];
+
+	for(var shapedInstanceItr = 0; shapedInstanceItr < maskedShapeInstanceList.length; shapedInstanceItr++) {
+		maskedShapeInstanceList[shapedInstanceItr].mask = mask_1;
+	}
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(25));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = null;
+// library properties:
+lib.properties = {
+	width: 200,
+	height: 200,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/Fanzlogoalone.png?1524519969793", id:"Fanzlogoalone"}
+	],
+	preloads: []
+};
+
+
+
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{}, AdobeAn = AdobeAn||{});
+var lib, images, createjs, ss, AdobeAn;
